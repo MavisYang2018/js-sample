97be16234de853104182a8d6abb5d35612451187
joe_ke <joe_ke@tpct.com.tw>
2018-05-04 09:44:04 +0800

    add isExist.js

---
+++ isExist.js
@@ -0,0 +1,15 @@
+var a = [5,1,10,2,9,4,8,7,4,6,3];
+
+function isExist (arr,t) {
+	var i = 0;
+	while (i < arr.lenght) {
+		if (arr[i] == t) {
+			return true;
+		}
+	}
+	return false
+}
+
+var b = isExist(a,11);
+
+console.log(b);
